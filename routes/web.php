99a746d765f840fa2326476406af4011bf10b3b8
hoangtai2003 <94899016+hoangtai2003@users.noreply.github.com>
2023-08-16 15:49:09 +0700

    shopping laravel

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/home', function () {
+    return view('home');
+});
+Route::prefix('categories')->group(function () {
+    Route::get('/create', [
+       'as' => 'categories.create',
+       'uses' => 'App\Http\Controllers\CategoryController@create'
+    ]);
+});
