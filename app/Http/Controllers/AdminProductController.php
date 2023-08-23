10236f3c866d1681036fb0166455b622cead9975
hoangtai2003 <94899016+hoangtai2003@users.noreply.github.com>
2023-08-24 01:42:48 +0700

    add prodct

--- app/Http/Controllers/AdminProductController.php
+++ app/Http/Controllers/AdminProductController.php
@@ -6,14 +6,26 @@ use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Components\Recusive;
 use App\Traits\StorageImageTrait;
-use Storage;
+use App\Models\Product;
+use App\Models\ProductImage;
+use App\Models\ProductTag;
+use App\Models\Tag;
+
 class AdminProductController extends Controller
 {
     use StorageImageTrait;
     private $category;
-    public function __construct(Category $category)
+    private $product;
+    private $productImage;
+    private $tags;
+    private $productTag;
+    public function __construct(Category $category, Product $product, ProductImage $productImage, Tag $tags, ProductTag $productTag)
     {
         $this->category = $category;
+        $this->product = $product;
+        $this->productImage = $productImage;
+        $this->tags = $tags;
+        $this->productTag = $productTag;
     }
     public function index(){
         return view('admin.product.index');
@@ -29,15 +41,37 @@ class AdminProductController extends Controller
         return $htmlOption;
     }
     public function store(Request $request){
-        // $dataProductCreate = [
-        //     'name' => $request->name,
-        //     'price' => $request->price,
-        //     'content' => $request->contents,
-        //     'user_id' => auth()->id(),
-        //     'category' => $request->category_id
-        // ];
+        $dataProductCreate = [
+            'name' => $request->name,
+            'price' => $request->price,
+            'content' => $request->contents,
+            'user_id' => auth()->id(),
+            'category_id' => $request->category_id
+        ];
         $dataUploadFeatureImage = $this->storageTraitUpload($request, 'feature_image_path', 'product');
-        dd($dataUploadFeatureImage);
+        if (!empty( $dataUploadFeatureImage)){
+            $dataProductCreate['feature_image_name'] = $dataUploadFeatureImage['file_name'];
+            $dataProductCreate['feature_image_path'] = $dataUploadFeatureImage['file_path'];
+        }
+        $product = $this->product->create($dataProductCreate);
 
+        // insert data to product_images
+        if($request->hasFile('image_path')) {
+            foreach($request->image_path as $fileItem) {
+                $dataProductImageDetail = $this->storageTraitUploadMutiple($fileItem, 'product');
+                $product->images()->create([
+                    'image_path' => $dataProductImageDetail['file_path'],
+                    'image_name' => $dataProductImageDetail['file_name']
+                ]);
+            }
+        }
+
+        // insert tags for product
+        foreach($request->tags as $tagItem){
+            // insert to tags
+            $tagInstance = $this->tags->firstOrCreate(['name' => $tagItem]);
+            $tagIds[] = $tagInstance->id;
+        }
+        $product->tags()->attach($tagIds);
     }
 }
--- app/Models/product.php
+++ app/Models/product.php
@@ -5,7 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class product extends Model
+class Product extends Model
 {
-    use HasFactory;
+    protected $guarded = [];
+    public function images(){
+        return $this->hasMany(ProductImage::class, 'product_id');
+    }
+    public function tags(){
+        return $this->belongsToMany(Tag::class, 'product_tags', 'product_id', 'tag_id')->withTimestamps();
+    }
 }
