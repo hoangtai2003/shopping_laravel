7d12a2b43d26f247350cbbac92a38e265f55b51b
hoangtai2003 <94899016+hoangtai2003@users.noreply.github.com>
2023-08-27 09:13:50 +0700

    create table role-permission-role_user-permission_role

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -53,7 +53,7 @@
                                 <input type="password" name="password" id="password" class="form-control">
                             </div>
                             <div class="form-group">
-                                <label for="remember-me" class="text-info"><span>Remember me</span> <span><input id="remember-me" name="remember-me" type="checkbox"></span></label><br>
+                                <label for="remember-me" class="text-info"><span>Remember me</span><span><input id="remember-me" name="remember-me" type="checkbox"></span></label><br>
                                 <input type="submit" name="remember_me" class="btn btn-info btn-md" value="submit">
                             </div>
                         </form>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/admin', 'App\Http\Controllers\AdminController@loginAdmin');
 Route::post('/admin', 'App\Http\Controllers\AdminController@postLoginAdmin');
+Route::post('/login', function () {
+    return view('login');
+});
 Route::get('/home', function () {
     return view('home');
 });
